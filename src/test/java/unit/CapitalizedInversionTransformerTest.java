706954794c566db8100da6ea03326ea128373ad0
Bartłomiej Mumot <mumot1999@gmail.com>
2020-12-16 07:48:33 +0100

    refactor: change to parametrized unit test

--- src/test/java/unit/CapitalizedInversionTransformerTest.java
+++ src/test/java/unit/CapitalizedInversionTransformerTest.java
@@ -1,86 +1,40 @@
 package unit;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pl.put.poznan.transformer.logic.*;
 import pl.put.poznan.transformer.logic.base.Text;
 import pl.put.poznan.transformer.logic.base.TextTransformerInterface;
+
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CapitalizedInversionTransformerTest {
 
-    private Text text = null;
-    private static final String[] testStrings = {
-            "DuużOpRoJektów",
-            "",
-            "DDDDD",
-            "ddddd",
-            "-1",
-            "\n",
-            "\t\nalamakotA\t",
-            " D D"
-    };
-
-    private static final String[] ansStrings = {
-            "WótkEjOrPożuud",
-            "",
-            "DDDDD",
-            "ddddd",
-            "1-",
-            "\n",
-            "\tatokamala\n\t",
-            "d d "
-    };
-    private static int testNumber = 0;
-
-    @BeforeEach
-    public void setUp(){
-        text = new Text(new CapitalizedInversionTransformer(new Text(testStrings[testNumber])).getText());
-        System.out.println("Running test " + String.valueOf(testNumber + 1));
-    }
-    @Test
-    public void invCapTest1(){
-       Assertions.assertEquals(ansStrings[testNumber], text.getText());
+    static Stream<Arguments> transformsArgumentsProvider() {
+        return Stream.of(
+                arguments("DuużOpRoJektów", "WótkEjOrPożuud"),
+                arguments("", ""),
+                arguments("DDDDD", "DDDDD"),
+                arguments("ddddd", "ddddd"),
+                arguments("-1", "1-"),
+                arguments("\n", "\n"),
+                arguments("\t\nalamakotA\t", "\tatokamala\n\t"),
+                arguments(" D D", "d d ")
+        );
     }
 
-    @Test
-    public void invCapTest2(){
-        Assertions.assertEquals(ansStrings[testNumber], text.getText());
-    }
+    @ParameterizedTest(name = "should transform {0} into {1}")
+    @MethodSource("transformsArgumentsProvider")
+    void shouldTransform(String in, String out) {
+        String transformed_text = new CapitalizedInversionTransformer(new Text(in)).getText();
 
-    @Test
-    public void invCapTest3(){
-        Assertions.assertEquals(ansStrings[testNumber], text.getText());
-    }
-
-    @Test
-    public void invCapTest4(){
-        Assertions.assertEquals(ansStrings[testNumber], text.getText());
-    }
-
-    @Test
-    public void invCapTest5(){
-        Assertions.assertEquals(ansStrings[testNumber], text.getText());
-    }
-
-    @Test
-    public void invCapTest6(){
-        Assertions.assertEquals(ansStrings[testNumber], text.getText());
-    }
-
-    @Test
-    public void invCapTest7(){
-        Assertions.assertEquals(ansStrings[testNumber], text.getText());
-    }
-
-    @Test
-    public void invCapTest8(){
-        Assertions.assertEquals(ansStrings[testNumber], text.getText());
-    }
-
-    @AfterEach
-    public void tearDown() {
-        text = null;
-        testNumber++;
-
+        Assertions.assertEquals(transformed_text, out);
     }
 }
