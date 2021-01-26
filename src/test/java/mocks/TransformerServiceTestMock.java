8056febd8aa90472fdf2e14f28f3d8fb213901a2
Bartłomiej Mumot <mumot1999@gmail.com>
2021-01-26 09:12:15 +0100

    test: add more mock tests

--- src/test/java/mocks/TransformerServiceTestMock.java
+++ src/test/java/mocks/TransformerServiceTestMock.java
@@ -1,16 +1,19 @@
 package mocks;
 
-import org.apache.commons.collections.bag.TransformedSortedBag;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InOrder;
 import pl.put.poznan.transformer.logic.*;
-import pl.put.poznan.transformer.logic.base.Text;
-import pl.put.poznan.transformer.logic.base.TextTransformerInterface;
 
 import java.lang.reflect.InvocationTargetException;
-import java.time.Instant;
+import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class TransformerServiceTestMock {
 
@@ -75,4 +78,33 @@ public class TransformerServiceTestMock {
         inOrder.verify(mock).getTransformer(t[1]);
         inOrder.verify(mock).getTransformer(t[2]);
     }
+
+    static Stream<Arguments> transformProviderCheckArgumentsProvider() {
+        return Stream.of(
+                arguments(new String[]{"upper", "upper", "upper"}, new HashMap<String, Integer>(){{put("upper", 3);}}),
+                arguments(new String[]{"upper", "lower", "upper"}, new HashMap<String, Integer>(){{put("upper", 2);put("lower", 1);}}),
+                arguments(new String[]{"capitalize", "lower", "capitalize"}, new HashMap<String, Integer>(){{put("capitalize", 2);put("lower", 1);}}),
+                arguments(new String[]{"acronym", "repetition_elimination", "text_to_acronym", "upper", "latex"}, new HashMap<String, Integer>(){{put("acronym", 1);put("repetition_elimination", 1);put("text_to_acronym", 1);put("upper", 1);put("latex", 1);}}),
+                arguments(new String[]{"a"}, new HashMap<String, Integer>(){{put("a", 1);}}),
+                arguments(new String[]{"b"}, new HashMap<String, Integer>(){{put("b", 1);}}),
+                arguments(new String[]{"c"}, new HashMap<String, Integer>(){{put("c", 1);}}),
+                arguments(new String[]{"d"}, new HashMap<String, Integer>(){{put("d", 1);}})
+        );
+    }
+
+    @ParameterizedTest(name = "{0} => {1}")
+    @MethodSource("transformProviderCheckArgumentsProvider")
+    void checkProvider(String[] transformers, HashMap<String, Integer> shouldBeCalled) throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        TransformerProvider mock = mock(TransformerProvider.class);
+        doReturn(CapitalizeTransformer.class).when(mock).getTransformer(any());
+
+        TextTransformerService service = new TextTransformerService(mock);
+        service.transformText("foo", transformers);
+
+        for(String s : shouldBeCalled.keySet()){
+            verify(mock, times(shouldBeCalled.get(s))).getTransformer(s);
+        }
+
+    }
+
 }
